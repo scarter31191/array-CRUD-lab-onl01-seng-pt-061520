8824e3bbb932b251d2293963c0c288e2acb63120
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-21 03:44:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,38 @@
-def create_an_empty_array
-  
+def create_an_empty_array  #learn --fail-fast
+  []
 end
 
 def create_an_array
+  ["Basetball", "Soccer", "Football", "MMA"]
+end
+
+def add_element_to_end_of_array(sports, fighting = "Boxing")
+  sports = ["Basetball", "Soccer", "Football", "MMA"]
+  sports.push fighting
+end
+
+def add_element_to_start_of_array(sports, fighting = "Boxing")
+  sports = ["Basetball", "Soccer", "Football", "MMA"]
+  sports.unshift fighting
   
 end
 
-def add_element_to_end_of_array(array, element)
-  
-end
-
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(sports)
+  sports.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
